cf7e25e71e757b0cdfb4d69ef4fe0aace7f9fbff
Azzovedi Luca <azzovedi.luca@osdbsesto>
2022-05-10 13:33:47 +0200

    loop

--- 3BIFirstJava/src/pkg3bifirstjava/Main.java
+++ 3BIFirstJava/src/pkg3bifirstjava/Main.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg3bifirstjava;
 
-/**
- *
- * @author azzovedi.luca
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int m=1;
+        for (int i=0; i<m; i++){
+            System.out.println(i);
+            m=m+1;
+        }
     }
     
 }
